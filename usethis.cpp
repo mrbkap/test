b1adb0ac4b6648c68a48ca9ae9c5397f53b886c0
Blake Kaplan <mrbkap@gmail.com>
2008-04-21 17:48:43 -0500

    Testing isspace
    committer: Blake Kaplan <mrbkap@gmail.com>
    
    --HG--
    extra : convert_revision : c6a26f995d3d51e2191d03f39e83fec406fc3fb8

--- usethis.cpp
+++ usethis.cpp
@@ -1,78 +1,15 @@
-#include <cstdio>
 #include <cctype>
 #include <iostream>
-#include <fstream>
-#include <cassert>
 using namespace std;
-
-void
-c_stdio(const char *fname)
-{
-    FILE *f, *out;
-    int i;
-    int numnodes, numedges;
-    int edge1, edge2;
-    int c;
-
-    f = fopen(fname, "r");
-    out = fopen("c_out", "w");
-    if (!f || !out) {
-        printf("Unable to open %s\n", f ? "c_out" : fname);
-        return;
-    }
-
-    i = 0;
-    while (fgetc(f) == 'g') {
-        fscanf(f, "%d %d", &numnodes, &numedges);
-        fprintf(out, "Graph %d has %d nodes and %d edges\n", i++, numnodes, numedges);
-
-        while (numedges--) {
-            fscanf(f, "%d %d", &edge1, &edge2);
-            fprintf(out, "Read edge (%d, %d)\n", edge1, edge2);
-        }
-
-        while (isspace(c = fgetc(f)))
-            continue;
-        if (c != EOF)
-            ungetc(c, f);
-    }
-
-    fclose(f);
-}
-
-void
-cpp_streams(const char *fname)
-{
-    ifstream f(fname);
-    ofstream out("cpp_streams");
-
-    char ch;
-    int i = 0;
-    while (f >> ch) {
-        assert(ch == 'g');
-
-        int numnodes, numedges;
-        f >> numnodes >> numedges;
-
-        out << "Graph " << i++ << " has " << numnodes << " nodes and " << numedges << " edges\n";
-        while (numedges--) {
-            int edge1, edge2;
-            f >> edge1 >> edge2;
-            out << "Read edge (" << edge1 << ", " << edge2 << ")\n";
-        }
-    }
-}
 
 int
 main(int argc, const char *argv[])
 {
-    if (argc != 2) {
-        printf("Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
+    int i = EOF;
 
-    c_stdio(argv[1]);
-    cpp_streams(argv[1]);
+    cout << "Answer: " << boolalpha << static_cast<bool>(isspace(i)) << '\n';
 
+    i = ' ';
+    cout << "Answer: " << boolalpha << static_cast<bool>(isspace(i)) << '\n';
     return 0;
 }
