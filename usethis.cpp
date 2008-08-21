6bcf651e80c174dde160859c750028638089b28f
Blake Kaplan <mrbkap@gmail.com>
2008-08-21 13:59:35 -0700

    The meaning of xkcd?

--- usethis.cpp
+++ usethis.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
-#include <set>
-using namespace std;
-
 int
 main(int argc, const char *argv[])
 {
-    set<int> s;
-
-    s.insert(4);
-    s.insert(5);
-    s.insert(2);
-    s.insert(1);
-    s.insert(9);
-
-    for (set<int>::const_iterator i = s.begin(); i != s.end(); ++i)
-        cout << *i << "\n";
+    std::cout << static_cast<int>('x' + 'k' + 'c' + 'd') - (4 * 'a' - 4) << '\n';
     return 0;
 }
