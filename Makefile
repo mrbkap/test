8aa39275b55e2a0040201f8900828f88216d8386
Blake Kaplan <mrbkap@gmail.com>
2009-06-05 12:01:46 -0700

    gdb spew thingi

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
+CXXFLAGS=-lpthread
+
 all: usethis
 
 usethis: usethis.cpp
 
 asm: usethis.cpp
-	g++ -S $(CFLAGS) usethis.cpp
+	g++ -S $(CXXFLAGS) usethis.cpp
 
 clean:
 	rm -f usethis usethis.s *~
--- usethis.cpp
+++ usethis.cpp
@@ -1,13 +1,76 @@
-#include <stdio.h>
-#include <stdint.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+using namespace std;
+
+void
+expected(int ch, int expected)
+{
+    if (ch != expected) {
+        ostringstream os;
+        os << "Unexpected character: " << static_cast<char>(ch) << " (" << ch << ")\n";
+        throw os.str();
+    }
+}
+
+size_t col = 0;
+
+char
+next()
+{
+    int ch = cin.get();
+    if (ch == EOF) {
+        string error = "Unexpected EOF\n";
+        throw error;
+    }
+
+    ++col;
+    return ch;
+}
 
 int
 main()
 {
-    int32_t new_size = 0;
+    try {
+        int ch;
+        while ((ch = cin.get()) != EOF) {
+            ++col;
+            while (isspace(ch)) {
+                ch = cin.get();
+                ++col;
+                if (ch == EOF) {
+                    return EXIT_SUCCESS;
+                }
+            }
+            expected(ch, '\'');
+            ch = next();
+            if (ch == '\\') {
+                switch (ch = next()) {
+                    case 'n':
+                        ch = '\n';
+                        break;
+                    case 't':
+                        ch = '\t';
+                        break;
+                    case '\'':
+                        ch = '\'';
+                        break;
+                    default: {
+                        string error = "Unknown escape sequence: ";
+                        error += static_cast<char>(ch);
+                        error += '\n';
+                        throw error;
+                    }
+                }
+            }
 
-    new_size = 0xff;
+            cout << static_cast<char>(ch);
+            expected(next(), '\'');
+        }
+    } catch (const std::string &str) {
+        cerr << "Oops " << col << "! " << str;
+    }
 
-    printf("0x%08x\n", new_size);
-    return 0;
+    return EXIT_SUCCESS;
 }
